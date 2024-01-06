1dc8152837f05ca7925ae15fb4ff6303b070a1ba
JKB0DY <virtmntm@gmail.com>
2024-01-06 15:56:12 +0100

    changed style sheet language

--- Angular-webapp/src/app/app.component.spec.ts
+++ Angular-webapp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular-webapp' title`, () => {
+  it(`should have the 'Angular-Webapp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-webapp');
+    expect(app.title).toEqual('Angular-Webapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-webapp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-Webapp');
   });
 });
