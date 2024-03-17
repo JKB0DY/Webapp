4456d84a0af1b76af295bf99448bfd944e217fd5
JKB0DY <virtmntm@gmail.com>
2024-03-17 23:25:30 +0100

    added usage of config service

--- Angular-webapp/src/app/hardware/services/hardware.service.ts
+++ Angular-webapp/src/app/hardware/services/hardware.service.ts
@@ -1,5 +1,7 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {HardwareList} from '../hardware';
+import {APP_SERVICE_CONFIG} from '../../AppConfig/appconfig.service';
+import {AppConfig} from '../../AppConfig/appconfig.interface';
 
 @Injectable({
 	providedIn: 'root',
@@ -35,7 +37,8 @@ export class HardwareService {
 			zustandBeschreibung: 'keine Kratzer',
 		},
 	];
-	constructor() {
+	constructor(@Inject(APP_SERVICE_CONFIG) private config: AppConfig) {
+		console.log(this.config.apiEndpoint);
 		console.log('HardwareService created');
 	}
 
