16479a254766d41a58f7b72a41ebccd67e97c8c6
JKB0DY <virtmntm@gmail.com>
2024-03-19 18:25:53 +0100

    added Httpclientmodule/provider

--- Angular-webapp/src/app/app.config.ts
+++ Angular-webapp/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
+import {provideHttpClient} from '@angular/common/http';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
@@ -8,6 +9,7 @@ import {APP_CONFIG, APP_SERVICE_CONFIG} from './AppConfig/appconfig.service';
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(routes),
+		provideHttpClient(),
 		provideClientHydration(),
 		{provide: APP_SERVICE_CONFIG, useValue: APP_CONFIG},
 	],
