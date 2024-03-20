0f0f03c57feda19b640aae37f338d2a9c6f7b71c
JKBODY <virtmntm@gmail.com>
2024-03-20 22:26:14 +0100

    added httpClient requirements

--- Angular-webapp/src/app/hardware/services/hardware.service.ts
+++ Angular-webapp/src/app/hardware/services/hardware.service.ts
@@ -2,6 +2,7 @@ import {Inject, Injectable} from '@angular/core';
 import {HardwareList} from '../hardware';
 import {APP_SERVICE_CONFIG} from '../../AppConfig/appconfig.service';
 import {AppConfig} from '../../AppConfig/appconfig.interface';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
 	providedIn: 'root',
@@ -37,7 +38,10 @@ export class HardwareService {
 			zustandBeschreibung: 'keine Kratzer',
 		},
 	];
-	constructor(@Inject(APP_SERVICE_CONFIG) private config: AppConfig) {
+	constructor(
+		@Inject(APP_SERVICE_CONFIG) private config: AppConfig,
+		private http: HttpClient
+	) {
 		console.log(this.config.apiEndpoint);
 		console.log('HardwareService created');
 	}
