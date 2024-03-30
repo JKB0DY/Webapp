7943814bcaefe0d236d09c0f864a8ff4800df45a
JKBODY <virtmntm@gmail.com>
2024-03-30 16:16:23 +0100

    Basics of Routing

--- Angular-webapp/src/app/app.component.ts
+++ Angular-webapp/src/app/app.component.ts
@@ -10,7 +10,7 @@ import {HardwareComponent} from './hardware/hardware.component';
 import {ContainerComponent} from './container/container.component';
 import {VeranstaltungComponent} from './veranstaltung/veranstaltung.component';
 import {LoggerService} from './logger.service';
-import {RouterModule} from '@angular/router';
+import {AppNavComponent} from './app-nav/app-nav.component';
 
 @Component({
 	selector: 'bltinv-root',
@@ -18,23 +18,23 @@ import {RouterModule} from '@angular/router';
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 	imports: [
-		RouterModule,
 		CommonModule,
 		HardwareComponent,
 		ContainerComponent,
 		VeranstaltungComponent,
+		AppNavComponent,
 	],
 })
 export class AppComponent implements OnInit {
-	title = 'Besondere Lernleistung';
+	// title = 'Besondere Lernleistung';
 
-	@ViewChild('name', {static: true}) name!: ElementRef;
+	// @ViewChild('name', {static: true}) name!: ElementRef;
 
 	constructor(@Optional() private loggerService: LoggerService) {}
 
 	ngOnInit(): void {
 		this.loggerService?.log('AppComponent initialized');
-		this.name.nativeElement.innerText = 'Besondere Lernleistung';
+		// this.name.nativeElement.innerText = 'Besondere Lernleistung';
 	}
 
 	// 	@ViewChild('user', {read: ViewContainerRef}) vcr!: ViewContainerRef;
--- Angular-webapp/src/app/app.routes.ts
+++ Angular-webapp/src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import {Routes} from '@angular/router';
 import {HardwareComponent} from './hardware/hardware.component';
 import {VeranstaltungComponent} from './veranstaltung/veranstaltung.component';
+import {NotfoundComponent} from './notfound/notfound.component';
+import {HardwareDetailsComponent} from './hardware/hardware-details/hardware-details.component';
 
 export const routes: Routes = [
 	{path: 'hardwarelist', component: HardwareComponent},
+	{path: 'hardwarelist/:id', component: HardwareDetailsComponent},
 	{path: 'veranstaltung', component: VeranstaltungComponent},
+	{path: 'not-found', component: NotfoundComponent},
 	{path: '', redirectTo: '/hardwarelist', pathMatch: 'full'},
-	{path: '**', redirectTo: '/hardwarelist'},
+	{
+		path: '**',
+		redirectTo: '/not-found',
+	},
 ];
