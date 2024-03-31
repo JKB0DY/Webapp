126dd93ff3b753b54890701ee9e0ed975283d929
JKBODY <virtmntm@gmail.com>
2024-03-31 17:03:49 +0200

    added update path

--- Angular-webapp/src/app/app.routes.ts
+++ Angular-webapp/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import {VeranstaltungComponent} from './veranstaltung/veranstaltung.component';
 import {NotfoundComponent} from './notfound/notfound.component';
 import {HardwareDetailsComponent} from './hardware/hardware-details/hardware-details.component';
 import {HardwareAddComponent} from './hardware/hardware-add/hardware-add.component';
+import {HardwareUpdateComponent} from './hardware/hardware-update/hardware-update.component';
 
 export const routes: Routes = [
 	{path: 'hardwarelist', component: HardwareComponent},
 	{path: 'hardwarelist/add', component: HardwareAddComponent},
+	{path: 'hardwarelist/edit/:id', component: HardwareUpdateComponent},
 	{path: 'hardwarelist/:id', component: HardwareDetailsComponent},
 	{path: 'veranstaltung', component: VeranstaltungComponent},
 	{path: 'not-found', component: NotfoundComponent},
