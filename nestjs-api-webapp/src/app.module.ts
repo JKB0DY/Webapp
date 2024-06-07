9eb92793e22ea2c70b9c4fbc60ba799b0606fedd
JKBODY <virtmntm@gmail.com>
2024-06-07 13:45:35 +0200

    formatting

--- nestjs-api-webapp/src/app.module.ts
+++ nestjs-api-webapp/src/app.module.ts
@@ -4,8 +4,8 @@ import {AuthModule} from './auth/auth.module';
 import {UserModule} from './user/user.module';
 import {HardwareModule} from './hardware/hardware.module';
 import {PrismaModule} from './prisma/prisma.module';
-import { VeranstaltungModule } from './veranstaltung/veranstaltung.module';
-import { VeranstaltungHardwareModule } from './veranstaltung-hardware/veranstaltung-hardware.module';
+import {VeranstaltungModule} from './veranstaltung/veranstaltung.module';
+import {VeranstaltungHardwareModule} from './veranstaltung-hardware/veranstaltung-hardware.module';
 
 @Module({
 	imports: [
